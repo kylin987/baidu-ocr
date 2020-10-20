94324dc9f406a9dcbd54fcb242b0bd159293b446
kylin87 <kylin87@qq.com>
2020-10-20 16:35:54 +0800

    baidu bug

---
+++ src/lib/AipSignOption.php
@@ -0,0 +1,20 @@
+<?php
+
+
+namespace baidu\ocr\lib;
+
+
+class AipSignOption
+{
+    const EXPIRATION_IN_SECONDS = 'expirationInSeconds';
+
+    const HEADERS_TO_SIGN = 'headersToSign';
+
+    const TIMESTAMP = 'timestamp';
+
+    const DEFAULT_EXPIRATION_IN_SECONDS = 1800;
+
+    const MIN_EXPIRATION_IN_SECONDS = 300;
+
+    const MAX_EXPIRATION_IN_SECONDS = 129600;
+}
